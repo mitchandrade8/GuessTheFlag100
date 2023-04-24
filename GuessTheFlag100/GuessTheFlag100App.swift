61b304989f47fad72e42171af721ff6280106bed
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-04-24 11:54:00 -0600

    Initial Commit

---
+++ GuessTheFlag100/GuessTheFlag100App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag100App.swift
+//  GuessTheFlag100
+//
+//  Created by Mitch Andrade on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
